50a16fba4d741afa8a5889c11f9ea15aac73ebc1
Prasad1912-design <moreprasad1836@gmail.com>
2026-03-03 16:08:35 +0530

    Ready

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('PrintNest Backend is Running 🚀');
+  res.send('PrintNest Backend is Runningsss 🚀');
 });
 
 //Before Login Routing
