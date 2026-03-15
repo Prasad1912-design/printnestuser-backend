87eb6060d3a9534574d5ca50eb07779ab195dc52
Prasad1912-design <moreprasad1836@gmail.com>
2026-03-16 02:28:40 +0530

    Commit Changes

---
+++ EntityClasses/afterLogin_entities/askQuery.js
@@ -0,0 +1,9 @@
+const { text } = require('express');
+const mongoose = require('mongoose');
+
+const askQyerySchema = new mongoose.Schema({
+  userId : {type : mongoose.Schema.Types.ObjectId, ref : 'RegisteredUser'},
+  query : {type : text}
+},{timestamps : true});
+
+module.exports = mongoose.model('askQuery', askQyerySchema);
---
+++ Routes/AfterLoginRouting/askQueryRouting.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const routes = express.Router();
+const checkToken = require('../../checkToken');
+const askQuery = require('../../EntityClasses/afterLogin_entities/askQuery');
+
+routes.post('/addNewQuery',checkToken, async (request,response)=>{
+
+  if (!request.body.query) {
+  return response.status(400).json({
+    success: false,
+    message: " Query is required."
+  });
+}
+
+  try
+  {
+
+  const InsertQuery = await askQuery.create({userId : request.user.id, query : request.body.query});
+
+    if(InsertQuery)
+    {
+      return response.status(200).json({success : true, message : "Your Query Successfully Submitted. We will contact you in short"});
+    }
+  }
+  catch(e)
+  {
+    return response.status(500).json({success : false, message : e.message});
+  }
+})
+
+modules.export = routes;
